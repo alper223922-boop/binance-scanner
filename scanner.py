9d670066569dc563c2f48daa7bc47d9399a45cc6
alper223922-boop <alper223922@gmail.com>
2026-06-10 22:52:49 +0300

    Update TELEGRAM_TOKEN in scanner.py

--- scanner.py
+++ scanner.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN   = os.getenv("-1003809715216", "YOUR_TOKEN")
+TELEGRAM_TOKEN   = os.getenv("8402488879:AAHbmCBU2JJS0fsKZyH6xY0SERzkWG-wqWM")
 TELEGRAM_CHAT_ID = os.getenv("1385442139", "YOUR_CHAT_ID")
 TIMEFRAME        = os.getenv("TIMEFRAME", "Min60")  # Istenebilir: Min15, Min60, Hour4
 TOP_RESULTS      = 10
