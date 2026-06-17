cbdb8f98488e856aca8432c004795582fd0fa56c
alper223922-boop <alper223922@gmail.com>
2026-06-17 19:00:29 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -19,8 +19,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN   = os.getenv("8402488879:AAHbmCBU2JJS0fsKZyH6xY0SERzkWG-wqWM", "")
-TELEGRAM_CHAT_ID = os.getenv("=1385442139", "")
+TELEGRAM_TOKEN   = os.getenv("TELEGRAM_TOKEN", "AAHbmCBU2JJS0fsKZyH6xY0SERzkWG-wqWM")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "AAHbmCBU2JJS0fsKZyH6xY0SERzkWG-wqWM")
 TIMEFRAME        = os.getenv("TIMEFRAME", "Min60")
 TOP_RESULTS      = 10
 MIN_VOLUME_USDT  = float(os.getenv("MIN_VOLUME_USDT", "1000000"))
